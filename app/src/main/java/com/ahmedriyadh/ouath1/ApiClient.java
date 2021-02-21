8ed878c65a09e328fe4aa0023f62bb426d81e5de
AhmedRiyadh441 <you@example.com>
2021-02-21 19:06:06 +0300

    init commit

--- app/src/main/java/com/ahmedriyadh/ouath1/ApiClient.java
+++ app/src/main/java/com/ahmedriyadh/ouath1/ApiClient.java
@@ -15,12 +15,14 @@ public class ApiClient {
     public static ApiInterface getApiInterface(boolean isShouldAddOauth1) {
         if (retrofit == null) {
 
-            // if you do not want add the token
-            // just leave it empty
-            // for the token secret make it null
-            OAuthInterceptor oauth1WooCommerce = new OAuthInterceptor.Builder().consumerKey("your_consumer_key_here")
+
+            OAuthInterceptor oauth1WooCommerce = new OAuthInterceptor.Builder()
+                    .consumerKey("your_consumer_key_here")
                     .consumerSecret("your_consumer_secret_here")
-                    .token("your_token_here").tokenSecret("your_token_secret_here").build();
+                    .token("your_token_here")
+                    .tokenSecret("your_token_secret_here")
+                    .isShouldExcludeOAuthToken(false)
+                    .build();
 
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
             interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
